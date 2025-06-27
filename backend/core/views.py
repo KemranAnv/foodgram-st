24d6b0d561ac0692c0127019c64480d0162daf2c
Annekov Kemran <kemran.anv@gmail.com>
2025-06-27 19:37:54 +0300

    Add Ingredient and Tag viewsets for read-only access with filtering.

---
+++ backend/core/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Ingredient, Tag
+from .serializers import IngredientSerializer, TagSerializer
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('name',)
+    pagination_class = None
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    pagination_class = None
